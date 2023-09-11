6c23caf9f8756a19ecad3df20f1035aa925ea9cd
Marten <marten.pipenberg@gmail.com>
2023-09-11 13:51:29 +0300

    commit

--- public/index.php
+++ public/index.php
@@ -1,5 +1,21 @@
 <?php
 
+spl_autoload_register(function ($className){
+    
+    $className = substr($className, 4);
+    var_dump($className);
+    require_once __DIR__ . "/../src/$className.php";
+});
+
+//
+use App\Animals\Dog as Doggo;
+
+$router = new App\Router();
+var_dump(new Doggo());
+
+
+
+
 //var_dump($_SERVER);
 switch($_SERVER['REQUEST_URI']) {
     case '/':
